37e7d1a48a4616ec676b37b7a956bcd89ad298a0
Asfandyar Abbasi <aabbasi@beedataanalytics.com>
2024-08-26 12:35:05 +0200

    Added comments

--- integration/utils.py
+++ integration/utils.py
@@ -10,6 +10,9 @@ OUR_API = "http://localhost:8266"
 
 
 def int_to_uuid(int_value: int) -> uuid.UUID:
+    """
+    Used to create a uuid from an int.
+    """
     # Ensure the integer value can fit into a 128-bit UUID
     if int_value < 0 or int_value >= 2 ** 128:
         raise ValueError("Integer value out of range for UUID")
@@ -21,27 +24,36 @@ def int_to_uuid(int_value: int) -> uuid.UUID:
     return str(uuid.UUID(bytes=int_bytes))
 
 
-def save_chat(event, logger, agent_id=None):
+def save_chat(event, logger):
+    """
+    Function responsible to creating a new chat.
+    """
     try:
         data = {"external_id": int_to_uuid(event["conversation_id"]), "agent_id": None,
                 "started_at": datetime.fromtimestamp(event["event_at"]).isoformat()}
 
         chat = requests.post(f"{OUR_API}/chats", json=data)
-        if chat.status_code==201:
+        if chat.status_code == 201:
             logger.info(f"Created chat {chat.json()['chat_id']}.")
         else:
             logger.error(f"Failed to create chat")
     except Exception as e:
         print(e)
 
+
 def patch_chat(event, logger):
+    """
+    Function responsible to sent a patch if the chat has been modified.
+    Its important to construct appropirte dictionaries
+    """
     try:
         data = {"external_id": int_to_uuid(event["conversation_id"])}
         data = {**data, "ended_at": event.get("event_at")} if event.get("event_name") == "END" else data
         if event.get("data") and event.get("data", {}).get("new_advisor_id"):
-            advisor = requests.get(f"{BIG_CHAT_API}/advisors/{event.get('data', {}).get('new_advisor_id')}")
+            advisor = requests.get(
+                f"{BIG_CHAT_API}/advisors/{event.get('data', {}).get('new_advisor_id')}")  # We need advisor email
             parameters = {"email": advisor.json()["email_address"]}
-            agent = requests.get(f"{OUR_API}/agents", params=parameters)
+            agent = requests.get(f"{OUR_API}/agents", params=parameters)  # We need agent uuid
             if len(agent.json()) == 0:
                 save_agent(event, logger)
                 agent = requests.get(f"{OUR_API}/agents", params=parameters)
@@ -64,7 +76,11 @@ def patch_chat(event, logger):
     except Exception as e:
         print(e)
 
+
 def save_agent(event, logger):
+    """
+    Function responsible to creating a new agent.
+    """
     try:
         advisor_id = event["data"].get("new_advisor_id")
         advisor = requests.get(f"{BIG_CHAT_API}/advisors/{advisor_id}")
@@ -82,22 +98,23 @@ def save_agent(event, logger):
 
 
 def save_message(event, logger):
+    """
+    Function to save a message of a chat.
+    """
     try:
         data = {"chat_id": int_to_uuid(event["conversation_id"]), "sent_at": event["event_at"],
-            "text": event["data"]["message"]}
+                "text": event["data"]["message"]}
 
         parameters = {"external_id": int_to_uuid(event["conversation_id"])}
-        chat = requests.get(f"{OUR_API}/chats", params=parameters)
+        chat = requests.get(f"{OUR_API}/chats", params=parameters)  # To check if the chat of the message already exists
 
+        # Save the chat ig doesnt exist.
         if chat.status_code != 200 or len(chat.json()) == 0:
             # conversation = requests.get(f"{BIG_CHAT_API}/conversations/{event['conversation_id']}")
 
             save_chat(event, logger)
-            chat = requests.get(f"{OUR_API}/chats", params=parameters)
-
             response = requests.post(f"{OUR_API}/chats/{int_to_uuid(event['conversation_id'])}/messages", json=data)
         else:
-
             response = requests.post(f"{OUR_API}/chats/{chat.json()[0]['chat_id']}/messages", json=data)
 
         if response.status_code == 200:
